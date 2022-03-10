ee20e37c428372d3bb93d3048f875122c2b1b78a
beecoding12345 <cammyreid10@icloud.com>
2022-03-10 10:48:34 +0000

    Made A Script In Unity Called HelloWorld

---
+++ Mobile Game Unity Project/Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,30 @@
+//HelloWorld
+//Cameron Reid
+//10/03/22
+//learn basics of unity scripts!
+
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//class contains evraything in the script
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //called auto by unity
+    void Start()
+    {
+        Debug.Log("Lexi Rivera");
+    }
+
+    // Update is called once per frame
+    //called auto by unity
+    void Update()
+    {
+        Debug.Log("Bukayo Saka");
+    }
+}
+//closing curly bracket ends the script
