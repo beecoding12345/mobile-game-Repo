33785e948ec7532ff7f9787e68ede99aefe217d2
beecoding12345 <cammyreid10@icloud.com>
2022-06-23 14:29:38 +0100

    updated

--- GameAssesment1/Assets/Scenes/Script/GameLosing.cs
+++ GameAssesment1/Assets/Scenes/Script/GameLosing.cs
@@ -1,18 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameLosing : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public void RestartButton(){
+        SceneManager.LoadScene("Game");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ExitButton()
     {
-        
+        SceneManager.LoadScene("MainMenu");
     }
 }
